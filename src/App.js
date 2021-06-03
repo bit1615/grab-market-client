2158cee8ee1c10225bbc09020b0e0d920402d205
Ji won Choi <jiwon@choijiwon-ui-MacBookPro.local>
2021-06-03 17:31:09 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPgae from "./upload";
+import ProductPgae from "./product";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />;
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPgae />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPgae />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
